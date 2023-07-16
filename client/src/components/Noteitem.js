674e9702a1ccaf5200a362ef9795ae8020b7b20c
Adi2895 <jangraaditya11@gmail.com>
2023-07-17 00:11:35 +0530

    noteitem modified

--- client/src/components/Noteitem.js
+++ client/src/components/Noteitem.js
@@ -1,23 +1,53 @@
-import React , {useContext}from 'react'
-import noteContext from '../context/notes/noteContext';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import noteContext from "../context/noteContext";
 
 const Noteitem = (props) => {
-    const context = useContext(noteContext);
-    const {deleteNote} = context;
-    const {note , updateNote } = props;
-    return (
-        <div className='col-md-3'>
-            <div className="card my-3" >
-                <div className="card-body">
-                    <div className="d-flex align-item-center">
-                        <h5 className="card-title">{note.title}</h5>
-                        <i onClick={()=>deleteNote(note._id)} className="fa-solid fa-trash-can-arrow-up mx-2 my-1"> </i>
-                        <i onClick={()=>updateNote(note)} className="fa-solid fa-pen-fancy mx-2 my-1"></i>
-                    </div>
-                    <p className="card-text">{note.description}</p>
-                </div>
-            </div>
+  const context = useContext(noteContext);
+  const { deleteNote } = context;
+  const { note, updateNote } = props;
+  
+  
+
+  return (
+    <div className="col-md-3">
+      <div className="card my-3">
+        <div className="card-body">
+          <div className="d-flex align-item-center">
+            <h5 style={{ color: "#000" }} className="card-title">
+              {note.title}
+            </h5>
+            <i
+              onClick={() => deleteNote(note._id)}
+              style={{ color: "#000" }}
+              className="fa fa-trash mx-2 my-1"
+            >
+              {" "}
+            </i>
+            <i
+              onClick={() => updateNote(note)}
+              style={{ color: "#000" }}
+              className="fa-solid fa-light fa-pen-to-square mx-2 my-1"
+            ></i>
+          </div>
+          <p style={{ color: "#000" }} className="card-text">
+            {note.description.length < 70 ? (
+              note.description
+            ) : (
+              <>
+                {note.description.slice(0, 50)}
+                <Link
+                  to={`/note/${note._id}`}
+                  style={{ color: "blue", textDecoration: "none" }}
+                >
+                  ...Show more{" "}
+                </Link>
+              </>
+            )}
+          </p>
         </div>
-    )
-}
-export default Noteitem
+      </div>
+    </div>
+  );
+};
+export default Noteitem;
