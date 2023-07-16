4ebf8dcdc34179fd20192fd1e6ca3feb9595c032
Adi2895 <jangraaditya11@gmail.com>
2023-07-17 00:14:54 +0530

    NoteState modified

--- client/src/context/NoteState.js
+++ client/src/context/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import noteContext from './noteContext';
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000"
+    const host = "http://localhost:3000"
     const initialNotes = []
 
     const [notes, setNotes] = useState(initialNotes);
